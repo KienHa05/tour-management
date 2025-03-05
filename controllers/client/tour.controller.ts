551550081dc22eb7b14e505ce4c54b34e56fe232
KienHa05 <kienhgph50150@gmail.com>
2025-03-05 12:19:15 +0700

    1.1 Tạo Khung Giao Diện
    BUỔI 62
    
    thực tế khung giao diện bên tours/detail.pug đã là bản copy, nên đã trả đủ data trước ở phần 1.2 rồi.

--- controllers/client/tour.controller.ts
+++ controllers/client/tour.controller.ts
@@ -5,35 +5,35 @@ import { QueryTypes } from "sequelize";
 
 // [GET] /tours/:slugCategory
 export const index = async (req: Request, res: Response) => {
-    // SELECT * FROM tours WHERE deleted = false AND status = "active";
-    // const tours = await Tour.findAll({
-    //     where: {
-    //         deleted: false,
-    //         status: "active"
-    //     },
-    //     raw: true
-    // });
+  // SELECT * FROM tours WHERE deleted = false AND status = "active";
+  // const tours = await Tour.findAll({
+  //     where: {
+  //         deleted: false,
+  //         status: "active"
+  //     },
+  //     raw: true
+  // });
 
 
-    const slugCategory = req.params.slugCategory;
-    /*
-    SELECT 
-      tours.*, 
-      price * (1 - discount / 100) AS price_special 
-    FROM 
-      tours 
-      JOIN tours_categories ON tours.id = tours_categories.tour_id 
-      JOIN categories ON tours_categories.category_id = categories.id 
-    WHERE 
-      categories.slug = 'du-lich-trong-nuoc' 
-      AND categories.deleted = false 
-      AND categories.status = 'active' 
-      AND tours.deleted = false 
-      AND tours.status = 'active';
-    
-    */
+  const slugCategory = req.params.slugCategory;
+  /*
+  SELECT 
+    tours.*, 
+    price * (1 - discount / 100) AS price_special 
+  FROM 
+    tours 
+    JOIN tours_categories ON tours.id = tours_categories.tour_id 
+    JOIN categories ON tours_categories.category_id = categories.id 
+  WHERE 
+    categories.slug = 'du-lich-trong-nuoc' 
+    AND categories.deleted = false 
+    AND categories.status = 'active' 
+    AND tours.deleted = false 
+    AND tours.status = 'active';
+  
+  */
 
-    const tours = await sequelize.query(`
+  const tours = await sequelize.query(`
     SELECT 
       tours.*, 
       ROUND(price * (1 - discount / 100), 0) AS price_special 
@@ -48,20 +48,29 @@ export const index = async (req: Request, res: Response) => {
       AND tours.deleted = false 
       AND tours.status = 'active';        
     `, {
-        type: QueryTypes.SELECT
-    });
+    type: QueryTypes.SELECT
+  });
 
-    tours.forEach(item => {
-        if (item["images"]) {
-            const images = JSON.parse(item["images"]);
-            item["image"] = images[0];
-        }
+  tours.forEach(item => {
+    if (item["images"]) {
+      const images = JSON.parse(item["images"]);
+      item["image"] = images[0];
+    }
 
-        item["price_special"] = parseFloat(item["price_special"]);
-    });
+    item["price_special"] = parseFloat(item["price_special"]);
+  });
 
-    res.render("client/pages/tours/index", {
-        pageTitle: "Danh Sách Tour",
-        tours: tours
-    });
+  res.render("client/pages/tours/index", {
+    pageTitle: "Danh Sách Tour",
+    tours: tours
+  });
+}
+
+// [GET] /tours/detail/:slugTour
+export const detail = async (req: Request, res: Response) => {
+  const slugTour = req.params.slugTour;
+
+  res.render("client/pages/tours/detail", {
+    pageTitle: "Chi Tiết Tour",
+  });
 }
