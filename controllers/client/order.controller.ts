8b0e89f35c4d09876a3a6dde01d88b539db85708
KienHa05 <kienhgph50150@gmail.com>
2025-03-06 17:18:34 +0700

    2.2 Server Lấy data lưu vào bảng orders
    BUỔI 64:
    
    Trong generate.ts đoạn code sau chưa cần dùng đến nên comment nó :
    // // Generate Mã tour
    // export const generateTourCode = (number: number): string => {
    //     const code = `TOUR${String(number).padStart(6, '0')}`;
    //     return code;
    // };

--- controllers/client/order.controller.ts
+++ controllers/client/order.controller.ts
@@ -1,12 +1,38 @@
 import { Request, Response } from "express";
+import Order from "../../models/order.model";
+import { generateOrderCode } from "../../helpers/generate";
 
 // [POST] /order/
 export const order = async (req: Request, res: Response) => {
     const data = req.body;
-    console.log(data);
+
+    // Lưu data vào bảng orders
+    const dataOrder = {
+        code: "",
+        fullName: data.info.fullName,
+        phone: data.info.phone,
+        note: data.info.note,
+        status: "initial"
+    };
+
+    const order = await Order.create(dataOrder);
+    const orderId = order.dataValues.id;
+
+    const code = generateOrderCode(orderId);
+    await Order.update(
+        {
+            code: code
+        },
+        {
+            where: {
+                id: orderId
+            }
+        }
+    );
 
     res.json({
         code: 200,
-        message: "Đặt Hàng Thành Công !"
+        message: "Đặt Hàng Thành Công !",
+        orderCode: code
     });
 };
